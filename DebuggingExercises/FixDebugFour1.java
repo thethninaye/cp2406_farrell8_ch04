c9e351e7e65d59340e04946c1fab635239710a5c
Thet Hnin Aye <thaye.thethnin@gmail.com>
2018-08-06 00:44:59 +0800

    Debug Task 3 Week 3

---
+++ DebuggingExercises/FixDebugFour1.java
@@ -0,0 +1,15 @@
+public class FixDebugFour1
+{
+   public static void main(String args[])
+   {
+      int x = 5;
+      int y = 8;
+      System.out.println("adding " + x + y);
+
+      int z = 19;
+      System.out.println("subtracting "  + (z - y));
+
+      System.out.println("dividing " + z / x);
+      System.out.println("multiplying " + x * z);
+   }
+}
